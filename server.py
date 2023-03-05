5a04c5d9546b7b11078508b59139e9031b45b17c
jeremy-feng <jeremy-feng@qq.com>
2023-03-05 22:49:51 +0800

    更新

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import dash
 
-app = dash.Dash(
-    __name__,
-    update_title=None,
-    suppress_callback_exceptions=True,
-)
+app = dash.Dash(__name__, update_title=None, suppress_callback_exceptions=True)
+
+app.title = "在线问答机器人"
